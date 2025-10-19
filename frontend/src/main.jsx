4bb9cd751583d44c26f225c720b4bf390a4ef091
parassadafule <parassadafule21@gmail.com>
2025-10-19 19:46:42 +0530

    fix index.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 
-// Uses Vite environment vars. Create .env with VITE_AUTH0_DOMAIN and VITE_AUTH0_CLIENT_ID
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

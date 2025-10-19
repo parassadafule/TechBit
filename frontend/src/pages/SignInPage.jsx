8e6c1e9ce0d4a4b1a91a934e5d96030c3ca12c85
vaibhavmashal <vaibhavmashal098@gmail.com>
2025-10-19 13:24:40 +0530

    db error resolved

--- frontend/src/pages/SignInPage.jsx
+++ frontend/src/pages/SignInPage.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const SignInPage = () => {
     const { loginWithRedirect, isLoading, error } = useAuth0();
-    const returnTo = '/signin';
+    const returnTo = '/signup-callback';
 
     return (
         <div className="signin-page">

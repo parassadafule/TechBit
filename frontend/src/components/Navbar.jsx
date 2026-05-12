a185b7a0629acb80c1c7b393b4dbd7a3a5fba7db
Atharavraj Dhumal <manasdhumal0@gmail.com>
2026-05-12 11:00:20 +0530

    feat:OG added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect } from 'react';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Link, useLocation } from 'react-router-dom';
@@ -52,7 +53,7 @@ const Navbar = () => {
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {}
-          <Link to="/" className="flex items-center space-x-2">
+          <Link to="/app" className="flex items-center space-x-2">
             <img src="/Logo.svg" alt="TechBit logo" className="w-8 h-8 rounded-lg" />
             <span className="text-xl font-bold text-gray-900">TechBit</span>
           </Link>
